f32d920a0c38c883fddd392730ea20adc970f023
AswinSTS <aswin.s@deltasoft.in>
2023-10-28 00:23:47 +0530

    frontend home screen done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import PrimarySearchAppBar from './Components/Header/Header'
+import OutlinedCard from './Components/Card/Card'
+import Sidebar from './Components/Sidebar/Sidebar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header style={{backgroundColor:"yellow"}}>
+        <PrimarySearchAppBar style={{backgroundColor:"yellow"}} />
       </header>
+      <main>
+        <div className='container-fluid mt-5'>
+          <div className='row'>
+            <div className='col-md-3 mt-5'>
+              <Sidebar className="mt-3" />
+            </div>
+            <div className='col-md-6 mt-5'>
+              <OutlinedCard />
+            </div>
+            <div className='col-md-3'>3</div>
+          </div>
+        </div>
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ frontend/src/Components/Card/Card.jsx
@@ -0,0 +1,54 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
+  >
+    •
+  </Box>
+);
+
+const card = (
+  <React.Fragment>
+    <CardContent>
+      <Typography sx={{ fontSize: 14 }} color="green" gutterBottom>
+        New
+      </Typography>
+      <Typography variant="h5" component="div">
+        MERN Stack Developer
+      </Typography>
+      <Typography sx={{ mb: 1.5 }} color="text.secondary">
+        Banglore, Karnataka
+      </Typography>
+      <Typography variant="body2">
+        In publishing and graphic design, Lorem ipsum is a placeholder text
+        commonly used to demonstrate the visual form of a document or a typeface
+        without relying on meaningful content. Lorem ipsum may be used as a
+        placeholder before final copy is available.
+        <br />
+        {'"a benevolent smile"'}
+      </Typography>
+    </CardContent>
+    <CardActions>
+      <Button size="small">View More</Button>
+    </CardActions>
+  </React.Fragment>
+);
+
+export default function OutlinedCard() {
+  return (
+    <Box sx={{ minWidth: 275 }}>
+      <Card variant="outlined" className="mt-2 cardBox">{card}</Card>
+      <Card variant="outlined" className="mt-2 cardBox">{card}</Card>
+      <Card variant="outlined" className="mt-2 cardBox">{card}</Card>
+      <Card variant="outlined" className="mt-2 cardBox">{card}</Card>
+    </Box>
+  );
+}
