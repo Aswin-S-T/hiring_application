434f56003b861654b191b354eda0606ae307c11d
AswinSTS <aswin.s@deltasoft.in>
2023-11-12 11:38:28 +0530

    small changes in login ui done

--- frontend/src/company/Screens/RegisterScreen.jsx
+++ frontend/src/company/Screens/RegisterScreen.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
 function RegisterScreen() {
+  const [role, setRole] = useState("candidate");
   return (
     <div>
       <div className="login-screen">
@@ -17,36 +18,66 @@ function RegisterScreen() {
           <div className="col-md-6 mt-5">
             <div className="container p-4 mt-5">
               <h2>Create new Account</h2>
-              <form className="mt-4">
-                <p>Company Name</p>
-                <input type="text" />
-                <br />
-                <br />
-                <p>Company Email Address</p>
-                <input type="email" />
-                <br />
-                <br />
-                <p>Contact Number</p>
-                <input type="text" />
-                <br />
-                <br />
-                <p>Type of Company</p>
-                <select style={{ width: "50%" }}>
-                  <option>Software</option>
-                  <option>Government</option>
-                </select>
-                <br />
-                <br />
+              {role == "company" ? (
+                <>
+                  <form className="mt-4">
+                    <p>Company Name</p>
+                    <input type="text" />
+                    <br />
+                    <br />
+                    <p>Company Email Address</p>
+                    <input type="email" />
+                    <br />
+                    <br />
+                    <p>Contact Number</p>
+                    <input type="text" />
+                    <br />
+                    <br />
+                    <p>Type of Company</p>
+                    <select style={{ width: "50%" }}>
+                      <option>Software</option>
+                      <option>Government</option>
+                    </select>
+                    <br />
+                    <br />
 
-                <p>Company Email Address</p>
-                <input type="email" />
-                <br />
-                <br />
-                <p>Password</p>
-                <input type="password" />
-                <br />
-                <button>Register</button>
-              </form>
+                    <p>Company Email Address</p>
+                    <input type="email" />
+                    <br />
+                    <br />
+                    <p>Password</p>
+                    <input type="password" />
+                    <br />
+                    <button>Register</button>
+                  </form>
+                </>
+              ) : (
+                <>
+                  <form className="mt-4">
+                    <p>First Name</p>
+                    <input type="text" />
+                    <br />
+                    <br />
+                    <p>Last Name</p>
+                    <input type="text" />
+                    <br />
+                    <br />
+                    <p>Email Address</p>
+                    <input type="email" />
+                    <br />
+                    <br />
+                    <p>Contact Number</p>
+                    <input type="text" />
+                    <br />
+                    <br />
+
+                    <p>Password</p>
+                    <input type="password" />
+                    <br />
+                    <button>Register</button>
+                  </form>
+                </>
+              )}
               <br />
               <a href="/company/login">Already have an account ?</a>
               <br />
