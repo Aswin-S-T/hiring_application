53d49290d3111f98110ec41557cf027b4caa3af7
AswinSTS <aswin.s@deltasoft.in>
2023-11-24 22:22:10 +0530

    small change

--- frontend/src/company/Components/Table2.jsx
+++ frontend/src/company/Components/Table2.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import API_ENDPOINTS from "../../Api";
+import Loader from "../../Components/Loader";
 
 const itemsPerPage = 2;
 
@@ -35,123 +36,133 @@ const Table2 = () => {
 
   return (
     <div>
-      <table
-        style={{ width: "100%", borderCollapse: "collapse", marginTop: "20px" }}
-      >
-        <thead>
-          <tr>
-            <th style={{ border: "1px solid #ddd", padding: "8px" }}>ID</th>
-            <th style={{ border: "1px solid #ddd", padding: "8px" }}>
-              Job Title
-            </th>
-            <th style={{ border: "1px solid #ddd", padding: "8px" }}>
-              Location
-            </th>
-            <th style={{ border: "1px solid #ddd", padding: "8px" }}>
-              Experience Level
-            </th>
-            <th style={{ border: "1px solid #ddd", padding: "8px" }}>
-              Applied Candidates Count
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {currentItems.map((item, index) => (
-            <React.Fragment key={index}>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <table
+            style={{
+              width: "100%",
+              borderCollapse: "collapse",
+              marginTop: "20px",
+            }}
+          >
+            <thead>
               <tr>
-                <td style={{ border: "1px solid #ddd", padding: "8px" }}>
-                  {index + 1}
-                </td>
-                <td
-                  style={{
-                    border: "1px solid #ddd",
-                    padding: "8px",
-                    cursor: "pointer",
-                  }}
-                  onClick={() => setSelectedJob(item)}
-                >
-                  {item?.job?.jobTitle}
-                </td>
-                <td style={{ border: "1px solid #ddd", padding: "8px" }}>
-                  {item?.job?.location}
-                </td>
-                <td style={{ border: "1px solid #ddd", padding: "8px" }}>
-                  {item?.job?.experienceLevel}
-                </td>
-                <td style={{ border: "1px solid #ddd", padding: "8px" }}>
-                  {item?.applied_users?.length}
-                </td>
+                <th style={{ border: "1px solid #ddd", padding: "8px" }}>ID</th>
+                <th style={{ border: "1px solid #ddd", padding: "8px" }}>
+                  Job Title
+                </th>
+                <th style={{ border: "1px solid #ddd", padding: "8px" }}>
+                  Location
+                </th>
+                <th style={{ border: "1px solid #ddd", padding: "8px" }}>
+                  Experience Level
+                </th>
+                <th style={{ border: "1px solid #ddd", padding: "8px" }}>
+                  Applied Candidates Count
+                </th>
               </tr>
-              {selectedJob && selectedJob.job._id === item.job._id && (
-                <>
-                  <h6>Applied candiates</h6>
+            </thead>
+            <tbody>
+              {currentItems.map((item, index) => (
+                <React.Fragment key={index}>
                   <tr>
-                    <td colSpan="5">
-                      {/* Render the expanded table with applied candidates */}
-                      <table
-                        style={{
-                          width: "100%",
-                          borderCollapse: "collapse",
-                          marginTop: "10px",
-                        }}
-                      >
-                        <thead>
-                          <tr>
-                            <th>ID</th>
-                            <th>Username</th>
-                            <th>Email</th>
-                            {/* Add more columns as needed */}
-                          </tr>
-                        </thead>
-                        <tbody>
-                          {selectedJob.applied_users.map((user) => (
-                            <>
-                              <tr key={user._id}>
-                                <td>{user._id}</td>
-                                <td>{user.username}</td>
-                                <td>{user.email}</td>
-                                {/* Add more columns as needed */}
-                              </tr>
-                            </>
-                          ))}
-                        </tbody>
-                      </table>
+                    <td style={{ border: "1px solid #ddd", padding: "8px" }}>
+                      {index + 1}
+                    </td>
+                    <td
+                      style={{
+                        border: "1px solid #ddd",
+                        padding: "8px",
+                        cursor: "pointer",
+                      }}
+                      onClick={() => setSelectedJob(item)}
+                    >
+                      {item?.job?.jobTitle}
+                    </td>
+                    <td style={{ border: "1px solid #ddd", padding: "8px" }}>
+                      {item?.job?.location}
+                    </td>
+                    <td style={{ border: "1px solid #ddd", padding: "8px" }}>
+                      {item?.job?.experienceLevel}
+                    </td>
+                    <td style={{ border: "1px solid #ddd", padding: "8px" }}>
+                      {item?.applied_users?.length}
                     </td>
                   </tr>
-                </>
-              )}
-            </React.Fragment>
-          ))}
-        </tbody>
-      </table>
+                  {selectedJob && selectedJob.job._id === item.job._id && (
+                    <>
+                      <h6>Applied candiates</h6>
+                      <tr>
+                        <td colSpan="5">
+                          {/* Render the expanded table with applied candidates */}
+                          <table
+                            style={{
+                              width: "100%",
+                              borderCollapse: "collapse",
+                              marginTop: "10px",
+                            }}
+                          >
+                            <thead>
+                              <tr>
+                                <th>ID</th>
+                                <th>Username</th>
+                                <th>Email</th>
+                                {/* Add more columns as needed */}
+                              </tr>
+                            </thead>
+                            <tbody>
+                              {selectedJob.applied_users.map((user) => (
+                                <>
+                                  <tr key={user._id}>
+                                    <td>{user._id}</td>
+                                    <td>{user.username}</td>
+                                    <td>{user.email}</td>
+                                    {/* Add more columns as needed */}
+                                  </tr>
+                                </>
+                              ))}
+                            </tbody>
+                          </table>
+                        </td>
+                      </tr>
+                    </>
+                  )}
+                </React.Fragment>
+              ))}
+            </tbody>
+          </table>
 
-      <div style={{ marginTop: "20px", float: "right" }}>
-        <button
-          className="prevBtn"
-          onClick={() => handlePageChange(currentPage - 1)}
-          disabled={currentPage === 1}
-        >
-          Previous
-        </button>
-        {Array.from({ length: totalPages }).map((_, index) => (
-          <button
-            key={index + 1}
-            onClick={() => handlePageChange(index + 1)}
-            style={{ marginLeft: "5px" }}
-            className="page-list-btn"
-          >
-            {index + 1}
-          </button>
-        ))}
-        <button
-          onClick={() => handlePageChange(currentPage + 1)}
-          disabled={currentPage === totalPages}
-          style={{ marginLeft: "5px" }}
-          className="nextBtn"
-        >
-          Next
-        </button>
-      </div>
+          <div style={{ marginTop: "20px", float: "right" }}>
+            <button
+              className="prevBtn"
+              onClick={() => handlePageChange(currentPage - 1)}
+              disabled={currentPage === 1}
+            >
+              Previous
+            </button>
+            {Array.from({ length: totalPages }).map((_, index) => (
+              <button
+                key={index + 1}
+                onClick={() => handlePageChange(index + 1)}
+                style={{ marginLeft: "5px" }}
+                className="page-list-btn"
+              >
+                {index + 1}
+              </button>
+            ))}
+            <button
+              onClick={() => handlePageChange(currentPage + 1)}
+              disabled={currentPage === totalPages}
+              style={{ marginLeft: "5px" }}
+              className="nextBtn"
+            >
+              Next
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 };
