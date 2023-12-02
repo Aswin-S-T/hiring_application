7c35dfe91a8d21c2b5954b1f91ad6caadc09ea69
AswinSTS <aswin.s@deltasoft.in>
2023-12-02 20:41:28 +0530

    ui of home page done

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,175 +1,38 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Navbar.css";
 function Navbar() {
-  const [userData, setUserData] = useState(
-    JSON.parse(localStorage.getItem("userData"))
-  );
-
-  const handleLogout = () => {
-    localStorage.removeItem("userData");
-    window.location.href = "/";
-  };
-
   return (
     <div>
-      <nav class="navbar navbar-expand-lg navbar-light">
-        <div className="container">
-          <a class="navbar-brand" href="/">
-            Hire Me
-          </a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item active">
-                <a class="nav-link" href="/">
-                  Home <span class="sr-only">(current)</span>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/alljobs">
-                  Jobs
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/myjobs">
-                  My jobs
-                </a>
-              </li>
-              <li class="nav-item">
-                {/* <a class="nav-link disabled">Disabled</a> */}
-                <a class="nav-link" href={`/interview/${userData?._id}`}>
-                  Interviews
-                </a>
-              </li>
-            </ul>
+      <nav class="navbar">
+        <div class="navbar-container container-fluid">
+          <input type="checkbox" name="" id="" />
+          <div class="hamburger-lines">
+            <span class="line line1"></span>
+            <span class="line line2"></span>
+            <span class="line line3"></span>
           </div>
-        </div>
-        <form class="form-inline">
-          {/* <input
-            class="form-control mr-sm-2"
-            type="search"
-            placeholder="Search"
-            aria-label="Search"
-          /> */}
-          <ul class="navbar-nav">
-            <div class="dropdown">
-              <button
-                class="dropdown-toggle btn mt-1"
-                type="button"
-                data-toggle="dropdown"
-                aria-expanded="false"
-              >
-                {userData && (
-                  <img src={userData?.profileImage} className="thumbnail" />
-                )}
-
-                {userData ? <>{userData.firstName}</> : <>Account</>}
-              </button>
-              <div class="dropdown-menu">
-                {userData ? (
-                  <>
-                    <a class="dropdown-item" href={`/profile/${userData?._id}`}>
-                      My Profile
-                    </a>
-                    <a class="dropdown-item" onClick={handleLogout}>
-                      Logout
-                    </a>
-                  </>
-                ) : (
-                  <>
-                    <a class="dropdown-item" href="/Login">
-                      Login
-                    </a>
-                  </>
-                )}
-              </div>
-            </div>
-            <li
-              class="nav-item active"
-              style={{ left: "-20px", position: "relative" }}
-            >
-              <a class="nav-link" href="#">
-                FAQ <span class="sr-only">(current)</span>
-              </a>
+          <ul class="menu-items">
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/alljobs">Jobs</a>
+            </li>
+            <li>
+              <a href="/my-jobs">My jobs</a>
+            </li>
+            <li>
+              <a href="#">Interviews</a>
+            </li>
+            <li>
+              <a href="/login">Login</a>
+            </li>
+            <li>
+              <a href="/register" className="loginBnt text-white">Sign Up</a>
             </li>
           </ul>
-          {/* <button class="SigninBtn my-2 my-sm-0" type="submit">
-            Login
-          </button> */}
-          {/* <button
-            type="button"
-            class="SigninBtn"
-            data-toggle="modal"
-            data-target="#staticBackdrop"
-          >
-            Login
-          </button> */}
-
-          <div
-            class="modal fade"
-            id="staticBackdrop"
-            data-backdrop="static"
-            data-keyboard="false"
-            tabindex="-1"
-            aria-labelledby="staticBackdropLabel"
-            aria-hidden="true"
-          >
-            <div class="modal-dialog modal-dialog-centered">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h5 class="modal-title text-center" id="staticBackdropLabel">
-                    User Login
-                  </h5>
-                  <button
-                    type="button"
-                    class="close"
-                    data-dismiss="modal"
-                    aria-label="Close"
-                  >
-                    <span aria-hidden="true">&times;</span>
-                  </button>
-                </div>
-                <div class="modal-body">
-                  <form>
-                    <p>Email</p>
-                    <input
-                      type="email"
-                      className="form-control w-100"
-                      placeholder="Email"
-                    />
-                    <br />
-                    <br />
-                    <p>Password</p>
-                    <input
-                      type="password"
-                      className="form-control w-100"
-                      placeholder="Email"
-                    />
-                    <button type="submit" className="mt-4 submitBtn">
-                      Login
-                    </button>
-                    <br />
-                    <div className="mt-3">
-                      <a>Create new Account?</a>
-                      <br />
-                      <a>Foreget password?</a>
-                    </div>
-                  </form>
-                </div>
-              </div>
-            </div>
-          </div>
-        </form>
+          <h1 class="logo">CareerLinker</h1>
+        </div>
       </nav>
     </div>
   );
